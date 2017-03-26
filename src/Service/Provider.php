702f20ea4168fe284d34eedba337438dd20f477d
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2017-03-26 18:19:59 +0200

    add the model loader extension and set cache on cache models

---
+++ src/Service/Provider.php
@@ -0,0 +1,41 @@
+<?php
+namespace SlaxWeb\Cache\Database\Service;
+
+use Pimple\Container;
+
+/**
+ * Cache Component Service Provider
+ *
+ * Registers the extended model loader.
+ *
+ * @package   SlaxWeb\Cache
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.1
+ */
+class Provider implements \Pimple\ServiceProviderInterface
+{
+    /**
+     * Register services
+     *
+     * Called when the container is about to register this provider. It defines
+     * all the required services for the Cache component.
+     *
+     * @param \Pimple\Container $app Service Container
+     * @return void
+     */
+    public function register(Container $app)
+    {
+        $app->extend(
+            "loadDBModel.service",
+            function(\SlaxWeb\Database\BaseModel $model, Container $app) {
+                if ($model instanceof \SlaxWeb\Cache\Database\Model) {
+                    $model->setCache($app["cache.service"]);
+                }
+                return $model;
+            }
+        );
+    }
+}
